c8ac2880ec7bb2019add592b8c5dd2ba724f4571
Benitmulindwa <mulindwabenit@gmail.com>
2023-12-27 01:15:32 +0100

    update project structure

---
+++ example.py
@@ -0,0 +1,27 @@
+from FletifyHTML.converter import convert_html_to_flet
+import flet as ft
+
+
+html_content = """
+        <div>
+            <h1>Title</h1>
+            <p>This is a paragraph.</p>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+            <a href="https://example.com">Visit Example</a>
+            <img src="https://picsum.photos/200/200?10" alt="Example Image">
+        </div>
+    """
+flet_code = convert_html_to_flet(html_content)
+
+
+def main(page: ft.Page):
+    # Example usage:
+
+    page.add(flet_code)
+    page.update()
+
+
+ft.app(target=main)
